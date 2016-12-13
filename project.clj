cbfd569d2248de1fab07aecc102c95f5cdc7cd8b
Christopher Brown <io@henrian.com>
2016-12-13 14:17:27 -0600

    Bump patch version and remove :scm from project config

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chbrown/twitter-api "0.7.10"
+(defproject chbrown/twitter-api "0.7.11"
   :description "Async interface to Twitter's REST and Streaming APIs"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
@@ -6,8 +6,6 @@
                  [http.async.client "0.5.2"]
                  [clj-oauth "1.5.5"]]
   :url "https://github.com/chbrown/twitter-api"
-  :scm {:name "git"
-        :url "https://github.com/chbrown/twitter-api"}
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/license/LICENSE-2.0.html"}
   :deploy-repositories [["clojars" {:sign-releases false}]]
